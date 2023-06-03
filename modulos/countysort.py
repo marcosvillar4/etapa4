3c6958f338bcfceea870955456030e53644b91b8
marcos <marcos.villar.mail@gmail.com>
2023-06-03 10:40:35 -0300

    coocking

--- modulos/countysort.py
+++ modulos/countysort.py
@@ -1,7 +1,12 @@
-def countysort(lista, path):
+def countysort(path, listavotos):
     import modulos.inforeader as info
     import csv
     import platform
+
+    print(listavotos)
+
+    def segundo_item(lista):
+        return lista[1]
 
     if platform.system() == 'Windows':
         csvfile = path + "\csv\\partidos.csv"           # Fix para linux
@@ -13,13 +18,15 @@ def countysort(lista, path):
     with open (csvfile, "r") as partidos:
         reader = csv.reader(partidos)
         for row in reader:
-            partid.append(row[2])
+            partid.append(int(row[2]))
 
     cantvotos = []
     for i in range(0, len(partid)):
-        cantvotos.append([int(partid[i]),lista.count(partid[i])])
+        cantvotos.append([int(partid[i]),listavotos.count(int(partid[i]))])
 
-    orden = sorted(cantvotos, key=info.inforeader(1, cantvotos))
+    print(cantvotos)
+
+    orden = sorted(cantvotos, key=segundo_item(cantvotos))
 
     mayor2 = []
     mayor2.append(orden[0])
