12607e3c3fdf9ec67667486b876cea9b883e6a08
marcos <marcos.villar.mail@gmail.com>
2023-06-03 11:04:06 -0300

    cook

--- modulos/votoswriter.py
+++ modulos/votoswriter.py
@@ -2,13 +2,15 @@ def gustavo(path, listavotos):
     import csv
     import platform
     import modulos.countysort as count
+    import modulos.filtro as filtro
 
     if platform.system() == 'Linux':
         csvfile = path + '\csv'
     elif platform.system() == 'Windows':
         csvfile = path + '/csv'
 
-    count.countysort(path, listavotos)
+    mayor = count.countysort(path, listavotos)
+    votosmayor = filtro.filtro(path, mayor)
     with open (csvfile, 'w') as votos:
         writer = csv.writer(votos)
 
