3b6189aefe8d680ffa86c9ae348a23dc6d3e13ca
marcos <marcos.villar.mail@gmail.com>
2023-06-03 10:20:14 -0300

    conunt y sprto

--- modulos/countysort.py
+++ modulos/countysort.py
@@ -1,8 +1,28 @@
-def countysort(lista):
+def countysort(lista, path):
     import modulos.inforeader as info
-    votoscount = []
-    listaord = [] 
-    listaord = sorted(lista, key=info(4))
-    print(listaord)
-    
-        
+    import csv
+    import platform
+
+    if platform.system() == 'Windows':
+        csvfile = path + "\csv\\partidos.csv"           # Fix para linux
+    elif platform.system() == 'Linux':
+        csvfile = path + '/csv/partidos.csv'
+
+
+    partid = []
+    with open (csvfile, "r") as partidos:
+        reader = csv.reader(partidos)
+        for row in reader:
+            partid.append(row[2])
+
+    cantvotos = []
+    for i in range(0, len(partid)):
+        cantvotos.append([int(partid[i]),lista.count(partid[i])])
+
+    orden = sorted(cantvotos, key=info.inforeader(1, cantvotos))
+
+    mayor2 = []
+    mayor2.append(orden[0])
+    mayor2.append(orden[1])
+
+    print(mayor2)
