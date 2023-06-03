71a9c4f347275c72daa260792c65fd7c94163059
marcos <marcos.villar.mail@gmail.com>
2023-06-03 11:49:12 -0300

    contador implementado

--- 1/main.py
+++ 1/main.py
@@ -2,8 +2,13 @@ import csv
 import os
 import modulos.votoswriter as votoswriter
 import modulos.votosreader as reader
+import modulos.contador as contador
 
 path = os.path.dirname(__file__)
 listavotos = reader.reader(path)
-votoswriter.gustavo(path, listavotos)
+mayor = votoswriter.gustavo(path, listavotos)
+cont1, cont2, conttotal = contador.contvotos(path, mayor)
+print(cont1)
+print(cont2)
+print(conttotal)
 print('carlitos')
