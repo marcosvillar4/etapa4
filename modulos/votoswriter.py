1c6f4077c9ffc142d224a2eb61fc9c476ef90fe7
marcos <marcos.villar.mail@gmail.com>
2023-06-03 10:22:12 -0300

    votoswriter tesdt count

--- modulos/votoswriter.py
+++ modulos/votoswriter.py
@@ -1,14 +1,15 @@
 def gustavo(path, listavotos):
     import csv
     import platform
+    import modulos.countysort as count
 
     if platform.system() == 'Linux':
         csvfile = path + '\csv'
     elif platform.system() == 'Windows':
         csvfile = path + '/csv'
 
-
+    count(listavotos, path)
     with open (csvfile, 'w') as votos:
         writer = csv.writer(votos)
-        
+
 
